8dccf1fa1c248c6b939a795882bdbd3d76cb0af1
andyCLY <lycheung@thinknkreate.com>
2023-08-25 19:02:01 +0800

    quick modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,50 @@
 #include <Arduino.h>
 #include <ezButton.h>
 
+/*-------------------define for pins-------------------*/
+
 #define MOTOR_CTRL_PIN_1 15 // Motorl Control Board PWM 1
 #define MOTOR_CTRL_PIN_2 16 // Motorl Control Board PWM 2
-#define PWM_PIN          4  // input pin for the potentiometer
+
+/*-------------------ezButton object-------------------*/
 
 ezButton limitSwitch_Up(37);   // create ezButton object that attach to pin 37
 ezButton limitSwitch_Down(38); // create ezButton object that attach to pin 38
-volatile int PWMValue = 0;     // PWM value to control the speed of motor
+
+/*----------------var for control motor----------------*/
+volatile bool motorHoming = true;   // will directly go to homing when true
+bool motorState = true;    // for checking the motor is moving Up or Down, ture=Up
 
 // Function prototypes
 bool limitSwitchTouched(ezButton limitSwitch);
-void motorUpAndDownTask(void * arg);
-void motorOnUp();
-void motorOnDown();
-void motorOff();
+void motorOn(int PWM);
+void motorP1(bool& state, uint8_t time=2);
+void motorP2(bool& state, uint8_t time=4);
+void motorCycle(void * arg);
+void homingRollerClamp(void * arg);
 
 void setup() {
   Serial.begin(115200);
 
   /*Create a task for running motor up and down continuously */
-  xTaskCreate(motorUpAndDownTask,
-              "Running Motor Up and Down continuously",
+  xTaskCreate(motorCycle,
+              "Running the Motor Cycle",
               4096,
               NULL,
-              24,  // highest priority task
+              12,  // highest priority task
               NULL);
-
-}
-
-void loop() {
-//   if (limitSwitchTouched(limitSwitch_Up)) {
-//     Serial.printf("Up Touched\n");
-//   }
-// //   else {
-// //     Serial.printf("Up Untouched\n");
-// //   }
-
-//   if (limitSwitchTouched(limitSwitch_Down)) {
-//     Serial.printf("Down Touched\n");
-//   }
-//   else {
-//     Serial.printf("Down Untouched\n");
-//   }
   
-//   delay(200);
+  // *Create a task for different kinds of little things
+  xTaskCreate(homingRollerClamp,      // function that should be called
+              "Homing roller clamp",  // name of the task (debug use)
+              4096,           // stack size
+              NULL,           // parameter to pass
+              13,             // task priority, 0-24, 24 highest priority
+              NULL);          // task handle
+
 }
+
+void loop() {/*should be nothing*/}
 
 bool limitSwitchTouched(ezButton limitSwitch) {
   limitSwitch.loop();
@@ -53,55 +52,91 @@ bool limitSwitchTouched(ezButton limitSwitch) {
   else return true;                         // touched
 }
 
-void motorOnUp() {
-  Serial.println("up");
-  // limitSwitch_Up.loop();   // MUST call the loop() function first
-
-  // if (limitSwitch_Up.getState()) { // untouched
-    // Read PWM value
-    PWMValue = analogRead(PWM_PIN);
-
-    analogWrite(MOTOR_CTRL_PIN_1, (PWMValue / 16)); // PWMValue: 0->4095
+// move the motor
+// set PWM to be positive to move up, negetive to move down, 0 to stop
+// max PWM is 255 (100%), half is 122 (50%)
+void motorOn(int PWM) {
+  if (PWM >= 0) {
+    analogWrite(MOTOR_CTRL_PIN_1, PWM);
     analogWrite(MOTOR_CTRL_PIN_2, 0);
-  // }
-  // else { // touched
-  //   motorOff();
-  // }
-}
-
-void motorOnDown() {
-  // Serial.println("down");
-  // limitSwitch_Down.loop();   // MUST call the loop() function first
-
-  // if (limitSwitch_Down.getState()) { // untouched
-    // Read PWM value
-    PWMValue = analogRead(PWM_PIN);
-
-    analogWrite(MOTOR_CTRL_PIN_2, (PWMValue / 16)); // PWMValue: 0->4095
+  } else {
+    PWM = abs(PWM);
     analogWrite(MOTOR_CTRL_PIN_1, 0);
-  // }
-  // else { // touched
-  //   motorOff();
-  // }
+    analogWrite(MOTOR_CTRL_PIN_2, PWM);
+  }
 }
 
-void motorOff() {
-  analogWrite(MOTOR_CTRL_PIN_1, 0);
-  analogWrite(MOTOR_CTRL_PIN_2, 0);
-}
-
-void motorUpAndDownTask(void * arg) {
-  for (;;) {
+// run the motor for pattern 1
+// state should be the motorState, can help to directly change (for checking in other place)
+// time is the number of time that pattern 1 should run, default is 2
+void motorP1(bool& state, uint8_t time) {
+  state = true; // motor move up first
+  if (state) {
+    motorOn(255);
     while (!limitSwitchTouched(limitSwitch_Up)) {
-      motorOnUp();
+      vTaskDelay(20);
     }
-    motorOff();
-
+    state = false;  // motor move down
+  } else {
+    motorOn(-76);
     while (!limitSwitchTouched(limitSwitch_Down)) {
-      motorOnDown();
+      vTaskDelay(20);
     }
-    motorOff();
-    
-    vTaskDelay(10);
+    state = true;
+    motorOn(0); // for safety, write the motor to stop, will overwrite soon
+  }
+
+  if (time > 0) { // run the next time
+    motorP1(state, time-1);
+  }
+}
+
+// run the motor for pattern 2
+// state should be the motorState, can help to directly change (for checking in other place)
+// time is the number of time that pattern 1 should run, default is 4
+void motorP2(bool& state, uint8_t time) {
+  state = true; // motor move up first
+  if (state) {
+    motorOn(51);
+    while (!limitSwitchTouched(limitSwitch_Up)) {
+      vTaskDelay(20);
+    }
+    state = false;  // motor move down
+  } else {
+    motorOn(-51);
+    while (!limitSwitchTouched(limitSwitch_Down)) {
+      vTaskDelay(20);
+    }
+    state = true;
+    motorOn(0); // for safety, write the motor to stop, will overwrite soon
+  }
+
+  if (time > 0) { // run the next time
+    motorP1(state, time-1);
+  }
+}
+
+void motorCycle(void * arg) {
+  for (;;) {
+    motorP1(motorState);
+    motorP2(motorState);
+  }
+}
+
+void homingRollerClamp(void * arg) {
+  for(;;) {
+    if (limitSwitch_Down.getStateRaw() == 0) {  // touched
+      motorHoming = false;
+      motorOn(0);
+    } else {
+      // motor move down
+      motorOn(-255);
+    }
+
+    while (!motorHoming) {
+      vTaskDelay(200);
+    }
+
+    vTaskDelay(50);
   }
 }
