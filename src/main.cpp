c71872f21a65b3ea77f1cd56c343a8a46785b83c
andyCLY <lycheung@thinknkreate.com>
2023-08-26 11:31:27 +0800

    fix: limit SW sense problem

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <ezButton.h>
 
+bool print = true;
+
 /*-------------------define for pins-------------------*/
 
 #define MOTOR_CTRL_PIN_1 15 // Motorl Control Board PWM 1
@@ -14,12 +16,13 @@ ezButton limitSwitch_Down(38); // create ezButton object that attach to pin 38
 /*----------------var for control motor----------------*/
 volatile bool motorHoming = true;   // will directly go to homing when true
 bool motorState = true;    // for checking the motor is moving Up or Down, ture=Up
+uint32_t recordTime;       // for record the time of motor 
 
 // Function prototypes
 bool limitSwitchTouched(ezButton limitSwitch);
 void motorOn(int PWM);
-void motorP1(bool& state, uint8_t time=2);
-void motorP2(bool& state, uint8_t time=4);
+void motorP1(uint8_t time=2);
+void motorP2(uint8_t time=4);
 void motorCycle(void * arg);
 void homingRollerClamp(void * arg);
 
@@ -44,7 +47,12 @@ void setup() {
 
 }
 
-void loop() {/*should be nothing*/}
+void loop() {
+  if (print) {
+    Serial.printf("Up:%d, Down:%d\n", limitSwitch_Up.getStateRaw(), limitSwitch_Down.getStateRaw());
+    print = false;
+  }
+}
 
 bool limitSwitchTouched(ezButton limitSwitch) {
   limitSwitch.loop();
@@ -69,57 +77,68 @@ void motorOn(int PWM) {
 // run the motor for pattern 1
 // state should be the motorState, can help to directly change (for checking in other place)
 // time is the number of time that pattern 1 should run, default is 2
-void motorP1(bool& state, uint8_t time) {
-  state = true; // motor move up first
+void motorP1(uint8_t time) {
+  static bool state = true; // motor move up first
   if (state) {
     motorOn(255);
-    while (!limitSwitchTouched(limitSwitch_Up)) {
+    do {
       vTaskDelay(20);
-    }
+    } while (limitSwitch_Up.getStateRaw() == 1);
     state = false;  // motor move down
   } else {
     motorOn(-76);
-    while (!limitSwitchTouched(limitSwitch_Down)) {
+    do {
       vTaskDelay(20);
-    }
+    } while (limitSwitch_Down.getStateRaw() == 1);
     state = true;
     motorOn(0); // for safety, write the motor to stop, will overwrite soon
   }
 
   if (time > 0) { // run the next time
-    motorP1(state, time-1);
+    Serial.printf("pattern 1 finish, %d times remains\n", time-1);
+    motorP1(time-1);
   }
 }
 
 // run the motor for pattern 2
 // state should be the motorState, can help to directly change (for checking in other place)
 // time is the number of time that pattern 1 should run, default is 4
-void motorP2(bool& state, uint8_t time) {
-  state = true; // motor move up first
+void motorP2(uint8_t time) {
+  static bool state = true; // motor move up first
   if (state) {
     motorOn(51);
-    while (!limitSwitchTouched(limitSwitch_Up)) {
-      vTaskDelay(20);
-    }
+      for (uint8_t i=0; i<50; ++i) { // total delay for 20*50=1000ms
+        vTaskDelay(20); 
+        if (limitSwitch_Up.getStateRaw() == 1) {
+          vTaskDelay(20); 
+        } else {
+          i=50; // if touch limit SW, directory go to next state
+        }
+      }
     state = false;  // motor move down
   } else {
     motorOn(-51);
-    while (!limitSwitchTouched(limitSwitch_Down)) {
+    do {
       vTaskDelay(20);
-    }
+    } while (limitSwitch_Down.getStateRaw() == 1);
     state = true;
     motorOn(0); // for safety, write the motor to stop, will overwrite soon
   }
 
   if (time > 0) { // run the next time
-    motorP1(state, time-1);
+  Serial.printf("pattern 2 finish, %d times remains\n", time-1);
+    motorP2(time-1);
   }
 }
 
 void motorCycle(void * arg) {
+  while (motorHoming) {
+    vTaskDelay(20);
+  }
+  Serial.println("homing completed");
   for (;;) {
-    motorP1(motorState);
-    motorP2(motorState);
+    motorP1();
+    motorP2();
   }
 }
 
@@ -135,6 +154,7 @@ void homingRollerClamp(void * arg) {
 
     while (!motorHoming) {
       vTaskDelay(200);
+      print = true;
     }
 
     vTaskDelay(50);
